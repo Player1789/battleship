b10ec999ebad29ae1feacd1967325c7530f90248
Jeahoon Jung <jhoon5245@gmail.com>
2022-10-16 16:00:21 +1300

    etc: add index.ts & App.tsx style

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ScoreBoard from "./components/ScoreBoard";
 import Ships from "./components/Ships";
 import StartModal from "./components/StartModal";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <ScoreBoard />
-      <Ships />
+    <div className={styles.app}>
+      <div className={styles.info}>
+        <ScoreBoard />
+        <Ships />
+      </div>
       <StartModal />
     </div>
   );
